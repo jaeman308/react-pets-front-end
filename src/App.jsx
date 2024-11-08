356819dd0b13b9440008bb73b0d34efb8aefbf47
jborruel308@me.com <jborruel308@me.com>
2024-11-07 19:34:27 -0800

    setup, add index and show pet data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-
-// src/App.jsx
+import {useState, useEffect} from 'react'
+import * as petService from './services/petService'
+import PetList from './components/PetList';
+import PetDetail from './components/PetDetail'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [petList, setPetList] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    const fetchPets = async() => {
+      try{
+        const pets = await petService.index()
+        console.log(pets)
+        if(pets.error){
+          throw new Error(pets.error)
+        }
+        setPetList(pets)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fetchPets()
+  }, [])
+
+  const updateSelected = (pet) => {
+    setSelected(pet)
+  }
+
+  return (
+  <>
+  <PetList petList={petList}  updateSelected={updateSelected}/>
+  <PetDetail selected={selected} />
+  </>
+  );
 };
 
 export default App;
